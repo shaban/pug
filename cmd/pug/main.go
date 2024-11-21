e90e7c0fb914f6a75babb0aa23cf7aaa0c3fa280
Shaban Naasso <mr.naasso@gmail.com>
2024-11-21 21:25:03 +0100

    Add more documentation

--- cmd/pug/main.go
+++ cmd/pug/main.go
@@ -76,16 +76,21 @@ func goImports(absPath string, src []byte) []byte {
 	return fmtOut
 }
 
-//
-
+// genFile takes a Pug template file, parses it,
+// generates the corresponding Go code, optimizes and formats the code,
+// and then writes the Go code to an output file.
+// parameters are the path to the template file for processing
+// and the output path where we want our generated go file
 func genFile(path, outdir string) {
 	log.Printf("\nfile: %q\n", path)
 
 	var (
+		//Splits the provided path into the directory portion (dir) and the filename portion (fname)
 		dir, fname = filepath.Split(path)
-		outPath    = outdir + "/" + fname
-		rx, _      = regexp.Compile("[^a-zA-Z0-9]+")
-		constName  = rx.ReplaceAllString(fname[:len(fname)-4], "")
+		//Combines the output directory (outdir) and the filename (fname) to create the full output path for the generated Go file.
+		outPath   = outdir + "/" + fname
+		rx, _     = regexp.Compile("[^a-zA-Z0-9]+")
+		constName = rx.ReplaceAllString(fname[:len(fname)-4], "")
 	)
 
 	wd, err := os.Getwd()
@@ -93,44 +98,55 @@ func genFile(path, outdir string) {
 		os.Chdir(dir)
 		defer os.Chdir(wd)
 	}
-
+	//Checks if a file with the same name (fname) has already been processed.
 	if _, ok := ns_files[fname]; ok {
+		//if a collision is found, it appends an underscore and a counter value
+		//to the filename and the constant name to avoid naming conflicts.
 		sfx := "_" + strconv.Itoa(len(ns_files))
+		//Updates the ns_files map to track the new filename.
 		ns_files[fname+sfx] = true
 		outPath += sfx
 		constName += sfx
 	} else {
+		//If no collision is found, it adds the filename to the ns_files map.
 		ns_files[fname] = true
 	}
-
+	//Reads the contents of the Pug template file (fname) into the fl byte slice.
 	fl, err := os.ReadFile(fname)
 	if err != nil {
 		log.Fatalln("cmd/pug: ReadFile(): ", err)
 	}
-
-	//
-
+	//Creates a new Pug parser (pug.New(path)) and uses it to parse the Pug template content (fl).
+	//The parsed template is stored in the jst variable.
 	jst, err := pug.New(path).Parse(fl)
 	if err != nil {
 		log.Fatalln("cmd/pug: pug.New(path).Parse(): ", err)
 	}
 
 	var (
-		bb  = new(bytes.Buffer)
+		bb = new(bytes.Buffer)
+		//Creates a new layout struct, which holds information about the Go code structure and imports.
 		tpl = newLayout(constName)
 	)
+	//Writes the initial part of the Go code (package declaration, imports, etc.) to the buffer bb.
 	tpl.writeBefore(bb)
 	before := bb.Len()
+	//Writes the Go code generated from the Pug template to the buffer bb.
 	jst.WriteIn(bb)
+	//When the template is empty (only consists of the initial part) we're done
 	if before == bb.Len() {
 		fmt.Print("generated: skipped (empty output)  done.\n\n")
 		return
 	}
+	//Writes the closing parts of the Go code to the buffer bb.
 	tpl.writeAfter(bb)
 
-	//
-
+	//Parses the generated Go code in the buffer bb using the parseGoSrc function.
+	//This creates an AST (Abstract Syntax Tree) representation of the Go code.
 	gst, err := parseGoSrc(outPath, bb)
+	//when there is an error write a report to __error.go
+	//with the error information and the buffer contents
+	//and exit the program indicating a parse error
 	if err != nil {
 		// TODO
 		bb.WriteString("\n\nERROR: parseGoSrc(): ")
@@ -138,16 +154,18 @@ func genFile(path, outdir string) {
 		os.WriteFile(outPath+"__Error.go", bb.Bytes(), 0644)
 		log.Fatalln("cmd/pug: parseGoSrc(): ", err)
 	}
-
+	//Collapses consecutive WriteString calls in the generated Go code to optimize string concatenation.
 	gst.collapseWriteString(inline, constName)
+	//Performs type checking on the generated Go code to catch potential errors.
 	gst.checkType()
+	//Checks for unresolved blocks in the generated Go code.
 	gst.checkUnresolvedBlock()
 
 	bb.Reset()
+	//Uses the goImports function to format the Go code and ensure proper imports.
 	fmtOut := goImports(outPath, gst.bytes(bb))
-
-	//
-
+	//Writes the formatted Go code (fmtOut) to the output file
+	//(outPath + ".go") with the specified permissions (0644).
 	err = os.WriteFile(outPath+".go", fmtOut, 0644)
 	if err != nil {
 		log.Fatalln("cmd/pug: WriteFile(): ", err)
